c9298c55654d761a6b34973ea279287d7a9dc325
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2020-08-15 16:04:52 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Allen Xie on 8/15/20.
+//  Copyright © 2020 Allen Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
